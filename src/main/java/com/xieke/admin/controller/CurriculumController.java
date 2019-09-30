c4d7042b70261cfff8a69de72c4e8913b1c4ff04
zhangyang <zhangyang@bz-ins.com>
2019-09-30 14:38:05 +0800

    修改controller返回类

--- src/main/java/com/xieke/admin/controller/CurriculumController.java
+++ src/main/java/com/xieke/admin/controller/CurriculumController.java
@@ -2,8 +2,8 @@ package com.xieke.admin.controller;
 
 import com.xieke.admin.bo.CurriculumBo;
 import com.xieke.admin.domain.CurriculumDomain;
+import com.xieke.admin.dto.ResultInfo;
 import com.xieke.admin.page.HtPage;
-import com.xieke.admin.util.HtResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,30 +20,30 @@ public class CurriculumController {
 
 
     @RequestMapping("/add")
-    public HtResult add(String curriculumName, Integer semester, Integer date, Integer year, Integer grade, Integer classTime, String price) {
+    public ResultInfo add(String curriculumName, Integer semester, Integer date, Integer year, Integer grade, Integer classTime, String price) {
         CurriculumBo curriculumBo = new CurriculumBo(curriculumName, semester, date, year, grade, classTime, new BigDecimal(price), 0, new Date(), "");
         if (!curriculumDomain.insert(curriculumBo)) {
-            return HtResult.error("新增失败");
+            return new ResultInfo<>("新增失败");
         }
-        return HtResult.success();
+        return new ResultInfo(true);
     }
 
     @RequestMapping("/delete")
-    public HtResult delete(Integer curriculumId) {
+    public ResultInfo delete(Integer curriculumId) {
         if (curriculumId == null) {
-            return HtResult.error("课程ID不能为空");
+            return new ResultInfo<>("课程ID不能为空");
         }
         if (!curriculumDomain.softDelete(curriculumId)) {
-            return HtResult.error("删除失败");
+            return new ResultInfo<>("删除失败");
         }
-        return HtResult.success();
+        return new ResultInfo(true);
     }
 
 
     @RequestMapping("/findPage")
-    public HtResult findPage(@RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex, @RequestParam(value = "pageSize", defaultValue = "20") Integer pageSize, Integer semester, Integer date, Integer year, Integer grade) {
+    public ResultInfo findPage(@RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex, @RequestParam(value = "pageSize", defaultValue = "20") Integer pageSize, Integer semester, Integer date, Integer year, Integer grade) {
         HtPage<CurriculumBo> htPage = curriculumDomain.findPage(pageIndex, pageSize, semester, date, year, grade);
-        return HtResult.success(htPage);
+        return new ResultInfo(htPage);
     }
 
 
--- src/main/java/com/xieke/admin/controller/StudentController.java
+++ src/main/java/com/xieke/admin/controller/StudentController.java
@@ -2,8 +2,8 @@ package com.xieke.admin.controller;
 
 import com.xieke.admin.bo.StudentBo;
 import com.xieke.admin.domain.StudentDomain;
+import com.xieke.admin.dto.ResultInfo;
 import com.xieke.admin.page.HtPage;
-import com.xieke.admin.util.HtResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,42 +19,41 @@ public class StudentController {
 
 
     @RequestMapping("/add")
-    public HtResult addStudent(String studentName, Integer sex, String phoneOne, String phoneOneRemark, String phoneTwo, String phoneTwoRemark, String school, Integer grade, Integer entranceYear) {
+    public ResultInfo addStudent(String studentName, Integer sex, String phoneOne, String phoneOneRemark, String phoneTwo, String phoneTwoRemark, String school, Integer grade, Integer entranceYear) {
         StudentBo studentBo = new StudentBo(studentName, sex, phoneOne, phoneOneRemark, phoneTwo, phoneTwoRemark, school, grade, entranceYear, 0, new Date(), "");
         if (!studentDomain.insert(studentBo)) {
-            return HtResult.error("新增失败");
+            return new ResultInfo<>("新增失败");
         }
-        return HtResult.success();
+        return new ResultInfo(true);
     }
 
     @RequestMapping("/delete")
-    public HtResult deleteStudent(Integer studentId) {
+    public ResultInfo deleteStudent(Integer studentId) {
         if (studentId == null) {
-            return HtResult.error("学生ID不能为空");
+            return new ResultInfo<>("学生ID不能为空");
         }
         if (!studentDomain.softDelete(studentId)) {
-            return HtResult.error("删除失败");
+            return new ResultInfo<>("删除失败");
         }
-        return HtResult.success();
+        return new ResultInfo(true);
     }
 
     @RequestMapping("/update")
-    public HtResult updateStudent(Integer studentId,String studentName, Integer sex, String phoneOne, String phoneOneRemark, String phoneTwo, String phoneTwoRemark, String school, Integer grade, Integer entranceYear){
-        if (studentId==null){
-            return HtResult.error("学生ID不能为空");
+    public ResultInfo updateStudent(Integer studentId, String studentName, Integer sex, String phoneOne, String phoneOneRemark, String phoneTwo, String phoneTwoRemark, String school, Integer grade, Integer entranceYear) {
+        if (studentId == null) {
+            return new ResultInfo<>("学生ID不能为空");
         }
-        if (!studentDomain.update(studentId, studentName, sex, phoneOne, phoneOneRemark, phoneTwo, phoneTwoRemark, school, grade, entranceYear)){
-            return HtResult.error("更新失败");
+        if (!studentDomain.update(studentId, studentName, sex, phoneOne, phoneOneRemark, phoneTwo, phoneTwoRemark, school, grade, entranceYear)) {
+            return new ResultInfo<>("更新失败");
         }
-        return HtResult.success();
+        return new ResultInfo(true);
     }
 
 
-
     @RequestMapping("/findPage")
-    public HtResult findPage(@RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex, @RequestParam(value = "pageSize", defaultValue = "20") Integer pageSize, Integer grade) {
+    public ResultInfo findPage(@RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex, @RequestParam(value = "pageSize", defaultValue = "20") Integer pageSize, Integer grade) {
         HtPage<StudentBo> htPage = studentDomain.findPage(pageIndex, pageSize, grade);
-        return HtResult.success(htPage);
+        return new ResultInfo(htPage);
     }
 
 

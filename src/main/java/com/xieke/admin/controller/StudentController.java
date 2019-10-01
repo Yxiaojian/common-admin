17a043782865155e2a3f190d17b74b5c8fe26cfc
zhangyang <zhangyang@bz-ins.com>
2019-10-01 10:16:43 +0800

    bugfix

--- src/main/java/com/xieke/admin/controller/StudentController.java
+++ src/main/java/com/xieke/admin/controller/StudentController.java
@@ -4,14 +4,15 @@ import com.xieke.admin.bo.StudentBo;
 import com.xieke.admin.domain.StudentDomain;
 import com.xieke.admin.dto.ResultInfo;
 import com.xieke.admin.page.HtPage;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.Date;
 
-@RestController
+@Controller
 @RequestMapping("/student")
 public class StudentController {
     @Resource
@@ -19,6 +20,7 @@ public class StudentController {
 
 
     @RequestMapping("/add")
+    @ResponseBody
     public ResultInfo addStudent(String studentName, Integer sex, String phoneOne, String phoneOneRemark, String phoneTwo, String phoneTwoRemark, String school, Integer grade, Integer entranceYear) {
         StudentBo studentBo = new StudentBo(studentName, sex, phoneOne, phoneOneRemark, phoneTwo, phoneTwoRemark, school, grade, entranceYear, 0, new Date(), "");
         if (!studentDomain.insert(studentBo)) {
@@ -28,6 +30,7 @@ public class StudentController {
     }
 
     @RequestMapping("/delete")
+    @ResponseBody
     public ResultInfo deleteStudent(Integer studentId) {
         if (studentId == null) {
             return new ResultInfo<>("学生ID不能为空");
@@ -39,6 +42,7 @@ public class StudentController {
     }
 
     @RequestMapping("/update")
+    @ResponseBody
     public ResultInfo updateStudent(Integer studentId, String studentName, Integer sex, String phoneOne, String phoneOneRemark, String phoneTwo, String phoneTwoRemark, String school, Integer grade, Integer entranceYear) {
         if (studentId == null) {
             return new ResultInfo<>("学生ID不能为空");
@@ -51,9 +55,19 @@ public class StudentController {
 
 
     @RequestMapping("/findPage")
+    @ResponseBody
     public ResultInfo findPage(@RequestParam(value = "pageIndex", defaultValue = "1") Integer pageIndex, @RequestParam(value = "pageSize", defaultValue = "20") Integer pageSize, Integer grade) {
         HtPage<StudentBo> htPage = studentDomain.findPage(pageIndex, pageSize, grade);
         return new ResultInfo(htPage);
+    }
+
+    @RequestMapping("/home")
+    public void toHome(){
+
+    }
+    @RequestMapping("/enroll")
+    public void toEnroll(){
+
     }
 
 

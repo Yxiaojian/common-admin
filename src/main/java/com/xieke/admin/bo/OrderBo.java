b8f54d9d2f91f85f55a9a6e808915ffe28d3949e
Frozen <zhangbaolin.1000@163.com>
2020-02-19 14:21:20 +0800

    订单管理 显示订单完成的日期和时间

--- src/main/java/com/xieke/admin/bo/OrderBo.java
+++ src/main/java/com/xieke/admin/bo/OrderBo.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -190,6 +191,7 @@ public class OrderBo implements Serializable {
     /**
      * 创建时间
      */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
     /**
